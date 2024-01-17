e4af9054fbc6b58612128f2c7483d4ee9100a768
Dzidic Sanja <dzidic.sanja@nsoft.com>
2024-01-17 09:49:33 +0100

    Display all types of resources in Modules Section

--- src/components/platform-page/modules/ModuleSection.vue
+++ src/components/platform-page/modules/ModuleSection.vue
@@ -2,73 +2,132 @@
   <v-sheet
     min-height="70vh"
     rounded="lg">
-    <v-card>
-      <v-card-title>
-        {{ activeModule?.name }}
-      </v-card-title>
-      <v-card-text>
-        {{ activeModule?.description }}
-      </v-card-text>
+    <v-card-title>
+      <h2>{{ activeModule?.product?.title }}</h2>
+      <h3>{{ activeModule?.title }}</h3>
+    </v-card-title>
+    <v-card-text>
+      {{ activeModule?.description }}
+    </v-card-text>
 
-      <v-container>
-        <v-row
-          v-for="resource in activeModule?.resources">
-          <div
-              v-if="resource.file_type === 'VIDEO'"
-              class="resource-video__row">
-            <video
-                class="resource-video__row"
-                id="player"
-                playsinline
-                controls>
-              <source
-                  :src="`${baseUrl}${resource.path}`"
-                  type="video/mp4" />
-            </video>
+    <v-container>
+      <div
+          v-for="video in videoResources">
+        <div class="resource-video__row">
+          <video
+              id="player"
+              playsinline
+              controls>
+            <source
+                v-if="video.path"
+                :src="`${baseUrl}${video.path}`"
+                type="video/mp4" />
+          </video>
+        </div>
+      </div>
+    </v-container>
+
+    <v-container>
+    <h2 v-if="imageResources?.length">Images</h2>
+    <div
+        class="images-container">
+      <div
+          v-for="image in imageResources">
+        <div class="resource-image__row">
+          <a
+            :href="`${baseUrl}${image.path}`"
+            data-caption="resource.display_name"
+            data-fancybox
+          >
+            <img
+              :src="`${baseUrl}${image.path}`"
+              width="200" height="150" alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
+    </v-container>
+
+    <v-container>
+      <h2 v-if="pdfResources?.length">PDF Files</h2>
+      <div
+          class="pdf-container">
+        <div
+            v-for="pdf in pdfResources">
+          <div class="resource-pdf__row">
+              <embed
+                  :src="`${baseUrl}${pdf.path}`"
+                  type="application/pdf"
+                  width="100%"
+                  height="100%"
+              />
           </div>
-          <div
-            v-if="resource.file_type === 'IMAGE'"
-            class="resource-image__row">
-            <a
-              :href="`${baseUrl}${resource.path}`"
-              data-caption="resource.display_name"
-              data-fancybox
-            >
-              <img
-                :src="`${baseUrl}${resource.path}`"
-                width="200" height="150" alt="" />
-            </a>
-          </div>
-        </v-row>
-      </v-container>
-    </v-card>
+        </div>
+      </div>
+    </v-container>
+
   </v-sheet>
 </template>
 
 <script setup>
-import { onMounted, ref} from 'vue';
+import {computed, onMounted, ref} from 'vue';
 import { storeToRefs } from 'pinia';
+
 import { usePlatformStore } from '../../../stores/platformStore.js';
+import { useStore } from '../../../stores/store.js';
 const { activeModule } = storeToRefs(usePlatformStore());
+const { baseUrl } = useStore();
 
 import Plyr from 'plyr';
-const player = ref();
+import 'plyr/dist/plyr.css';
+const player = ref(null);
 
-const baseUrl = ref('http://localhost:3000');
+import { Fancybox } from "@fancyapps/ui";
+import '@fancyapps/ui/dist/fancybox/fancybox.css';
+
 const dialog = ref(false);
 
-onMounted(() => {
-  player.value = new Plyr('#player');
-});
+const videoResources = computed(() =>
+  activeModule.value?.resources?.filter((resource) => resource.file_type === 'VIDEO'));
 
+const imageResources = computed(() =>
+  activeModule.value?.resources?.filter((resource) => resource.file_type === 'IMAGE'));
+
+const pdfResources = computed(() =>
+  activeModule.value?.resources?.filter((resource) => resource.file_type === 'PDF'));
+
+onMounted(() => {
+  setTimeout(() => player.value = new Plyr('#player'), 2000);
+  Fancybox.bind('[data-fancybox]', {});
+});
 </script>
+
 <style lang="scss" scoped>
 .resource-video__row {
-  width: 80%;
-  max-height: 40%;
-
   video {
-
+    border-radius: 1rem;
+    margin-left: 5%;
+    width: 90%;
+    max-height: 40rem;
   }
+}
+
+.images-container {
+  display:flex;
+  flex-flow: row wrap;
+}
+
+.resource-image__row {
+  margin: 1rem;
+}
+
+.pdf-container {
+  margin-top: 2rem;
+}
+
+.resource-pdf__row {
+  width: 90%;
+  height: 40rem;
+  margin-left: 5%;
 }
 </style>
